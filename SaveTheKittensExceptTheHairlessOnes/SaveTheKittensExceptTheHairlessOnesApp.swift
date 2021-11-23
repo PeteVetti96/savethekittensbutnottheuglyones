27ad4911985631c31d32653dd73ec205e96f3947
Peters-MacBook-Pro <corvettipeter@gmail.com>
2021-11-23 15:15:45 -0500

    Initial Commit

---
+++ SaveTheKittensExceptTheHairlessOnes/SaveTheKittensExceptTheHairlessOnesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveTheKittensExceptTheHairlessOnesApp.swift
+//  SaveTheKittensExceptTheHairlessOnes
+//
+//  Created by Peter Corvetti on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct SaveTheKittensExceptTheHairlessOnesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
